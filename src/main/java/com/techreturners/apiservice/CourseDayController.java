89921859628128170f1f8049c1d2420e224c92a8
James Heggs <james.heggs@techreturners.com>
2020-04-17 09:17:07 +0100

    Add instrumentation to Java app

--- src/main/java/com/techreturners/apiservice/CourseDayController.java
+++ src/main/java/com/techreturners/apiservice/CourseDayController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import io.micrometer.core.annotation.Timed;
 
 @RestController
 public class CourseDayController {
@@ -16,7 +17,8 @@ public class CourseDayController {
     @Autowired
     protected CourseDayService courseDayService = null;
 
-	@GetMapping("/course-day")
+    @GetMapping("/course-day")
+    @Timed("get.course-day")
 	public CourseDay getCourseDay(@RequestParam(defaultValue = "1") String day) {
         logger.info("getCourseDay() - START");
         CourseDay cd = courseDayService.getDay(Integer.parseInt(day));
