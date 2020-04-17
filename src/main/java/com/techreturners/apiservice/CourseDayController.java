daea138e9bf9d36ab6b3bbe83e85076206a4acf9
James Heggs <james.heggs@techreturners.com>
2020-04-17 07:50:39 +0100

    Add JSON logging to our app

--- src/main/java/com/techreturners/apiservice/CourseDayController.java
+++ src/main/java/com/techreturners/apiservice/CourseDayController.java
@@ -5,14 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 public class CourseDayController {
+
+    Logger logger = LoggerFactory.getLogger(CourseDayController.class);
 
     @Autowired
     protected CourseDayService courseDayService = null;
 
 	@GetMapping("/course-day")
 	public CourseDay getCourseDay(@RequestParam(defaultValue = "1") String day) {
-        return courseDayService.getDay(Integer.parseInt(day));
+        logger.info("getCourseDay() - START");
+        CourseDay cd = courseDayService.getDay(Integer.parseInt(day));
+        logger.info("getCourseDay() - END");
+        return cd;
 	}
 }
