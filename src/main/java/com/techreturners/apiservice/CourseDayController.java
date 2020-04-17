369781ec8cc39c650806fbd484c640a888f3c1c4
James Heggs <james.heggs@techreturners.com>
2020-04-17 13:38:01 +0100

    Revert back to vanilla app

--- src/main/java/com/techreturners/apiservice/CourseDayController.java
+++ src/main/java/com/techreturners/apiservice/CourseDayController.java
@@ -5,24 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import io.micrometer.core.annotation.Timed;
-
 @RestController
 public class CourseDayController {
-
-    Logger logger = LoggerFactory.getLogger(CourseDayController.class);
 
     @Autowired
     protected CourseDayService courseDayService = null;
 
     @GetMapping("/course-day")
-    @Timed("get.course-day")
 	public CourseDay getCourseDay(@RequestParam(defaultValue = "1") String day) {
-        logger.info("getCourseDay() - START");
-        CourseDay cd = courseDayService.getDay(Integer.parseInt(day));
-        logger.info("getCourseDay() - END");
-        return cd;
+        return courseDayService.getDay(Integer.parseInt(day));
 	}
 }
