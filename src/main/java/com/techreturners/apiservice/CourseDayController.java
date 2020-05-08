80d2356467f58259c691b75fcb5ed8a2e843a7b2
James Heggs <james.heggs@techreturners.com>
2020-05-08 11:51:00 +0100

    Add prometheus annotations

--- src/main/java/com/techreturners/apiservice/CourseDayController.java
+++ src/main/java/com/techreturners/apiservice/CourseDayController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import io.micrometer.core.annotation.Timed;
 
 @RestController
 public class CourseDayController {
@@ -17,6 +18,7 @@ public class CourseDayController {
 
 
     @GetMapping("/course-day")
+    @Timed("get.course-day")
 	public CourseDay getCourseDay(@RequestParam(defaultValue = "1") String day) {
         logger.info("getCourseDay() - START");
         CourseDay courseDay = courseDayService.getDay(Integer.parseInt(day));
