71fdbffdb27d330544ed1a5bca7962e2dcea2b91
James Heggs <james.heggs@techreturners.com>
2020-05-08 12:59:55 +0100

    Revert "Introduce JSON logging and info around start/end of method"
    This reverts commit 85ea0f18d9750231fa1500dfeb44347d3a11c97f.

--- src/main/java/com/techreturners/apiservice/CourseDayController.java
+++ src/main/java/com/techreturners/apiservice/CourseDayController.java
@@ -4,9 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import io.micrometer.core.annotation.Timed;
 
 @RestController
 public class CourseDayController {
@@ -14,15 +11,8 @@ public class CourseDayController {
     @Autowired
     protected CourseDayService courseDayService = null;
 
-    Logger logger = LoggerFactory.getLogger(CourseDayController.class);
-
-
     @GetMapping("/course-day")
-    @Timed("get.course-day")
 	public CourseDay getCourseDay(@RequestParam(defaultValue = "1") String day) {
-        logger.info("getCourseDay() - START");
-        CourseDay courseDay = courseDayService.getDay(Integer.parseInt(day));
-        logger.info("getCourseDay() - END");
-        return courseDay;
+        return courseDayService.getDay(Integer.parseInt(day));
 	}
 }
