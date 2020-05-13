4a9c2e788c7a57e79aa7e4047da67e2fb4fc8ba6
Jack Elliott <jackelliott@jackbookpro-2.home>
2020-05-13 21:02:31 +0100

    Adding micrometer metric logging

--- src/main/java/com/techreturners/apiservice/CourseDayController.java
+++ src/main/java/com/techreturners/apiservice/CourseDayController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import io.micrometer.core.annotation.Timed;
 
 @RestController
 public class CourseDayController {
@@ -15,7 +16,8 @@ public class CourseDayController {
     Logger logger = LoggerFactory.getLogger(CourseDayController.class);
 
     @GetMapping("/course-day")
-	public CourseDay getCourseDay(@RequestParam(defaultValue = "1") String day) {
+    @Timed("get.course-day")
+    public CourseDay getCourseDay(@RequestParam(defaultValue = "1") String day) {
       logger.info("getCourseDay() - START");
       CourseDay courseDay = courseDayService.getDay(Integer.parseInt(day));
       logger.info("getCourseDay() - END");
