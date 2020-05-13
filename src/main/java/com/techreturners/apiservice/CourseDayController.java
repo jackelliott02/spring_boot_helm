6aba03e4aeacc2dba9dd120a65bd1d2c2238a1ac
Jack Elliott <jackelliott@jackbookpro-2.home>
2020-05-13 20:47:20 +0100

    Adding logging

--- src/main/java/com/techreturners/apiservice/CourseDayController.java
+++ src/main/java/com/techreturners/apiservice/CourseDayController.java
@@ -4,15 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class CourseDayController {
 
     @Autowired
     protected CourseDayService courseDayService = null;
+    Logger logger = LoggerFactory.getLogger(CourseDayController.class);
 
     @GetMapping("/course-day")
 	public CourseDay getCourseDay(@RequestParam(defaultValue = "1") String day) {
-        return courseDayService.getDay(Integer.parseInt(day));
+      logger.info("getCourseDay() - START");
+      CourseDay courseDay = courseDayService.getDay(Integer.parseInt(day));
+      logger.info("getCourseDay() - END");
+      return courseDay;
 	}
 }
